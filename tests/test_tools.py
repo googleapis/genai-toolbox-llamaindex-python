cbf48f2a4e061458e3d29e04e640080da55c7065
Twisha Bansal <twishabansal@google.com>
2025-02-28 13:31:18 +0530

    lint

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -11,8 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from unittest.mock import Mock, patch
 import concurrent.futures
+from unittest.mock import Mock, patch
 
 import pytest
 from pydantic import BaseModel
